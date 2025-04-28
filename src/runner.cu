c36aa690fb5544fb89d9548ab7490de1e7f3bd9b
toast <toastts@icloud.com>
2025-04-27 20:10:33 -0700

    remove print statements

--- src/runner.cu
+++ src/runner.cu
@@ -35,9 +35,6 @@ void run_simple_gemm(int M, int N, int K, float alpha, float *A, float *B,
                      float beta, float *C) {
   dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
   dim3 blockDim(32, 32);
-  printf("Grid Dim: (%d, %d)\n", gridDim.x, gridDim.y);
-  printf("Block Dim: (%d, %d)\n", blockDim.x, blockDim.y);
-  printf("Total Threads: %d\n", gridDim.x * gridDim.y * blockDim.x * blockDim.y);
   simple_gemm<<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
 
@@ -45,9 +42,6 @@ void run_global_mem_gemm(int M, int N, int K, float alpha, float *A, float *B,
                     float beta, float *C) {
   dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
   dim3 blockDim(32 * 32);
-  printf("Grid Dim: (%d, %d)\n", gridDim.x, gridDim.y);
-  printf("Block Dim: (%d)\n", blockDim.x);
-  printf("Total Threads: %d\n", gridDim.x * gridDim.y * blockDim.x);
   global_mem_gemm<32><<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
 
